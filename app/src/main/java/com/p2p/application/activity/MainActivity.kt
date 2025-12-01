8541ae7ad78aade5148377fb85add851ad9ac60a
DeepakYesIt <yesitlabs.deepaka@gmail.com>
2025-12-01 18:26:44 +0530

    zdffd
    zdfbdfgb

--- app/src/main/java/com/p2p/application/activity/MainActivity.kt
+++ app/src/main/java/com/p2p/application/activity/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.p2p.application.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.p2p.application.R
 import com.p2p.application.util.SessionManager
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var sessionManager: SessionManager
     private var selectedType: String = ""
@@ -19,21 +18,17 @@ class MainActivity : AppCompatActivity() {
         sessionManager = SessionManager(this)
         selectedType = sessionManager.getLoginType().orEmpty()
         startDestination()
-
     }
-
-
     private fun startDestination() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.frameContainerMain) as NavHostFragment
         val navController = navHostFragment.navController
         val navGraph = navController.navInflater.inflate(R.navigation.main_graph)
-//        navGraph.setStartDestination(R.id.loginFragment)
         if (sessionManager.getIsLogin()?:false){
             navGraph.setStartDestination(R.id.userWelcomeFragment)
         }else{
             navGraph.setStartDestination(R.id.accountTypeFragment)
         }
-
         navController.graph = navGraph
     }
+    
 }
